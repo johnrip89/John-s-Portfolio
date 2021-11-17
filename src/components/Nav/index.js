342ce542dbb9e8d3649c60daf96f156eb1360327
johnrip89 <lebronjohn89@gmail.com>
2021-11-17 11:04:57 -0700

    created files for About, Contact, and Resume

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,13 +7,16 @@ function Nav() {
         <div class="col-7">
             <ul class="nav nav-tabs">
                 <li class="nav-item">
-                    <a class="nav-link" href="">Home</a>
+                    <a class="nav-link" href="">About Me</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#contact">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#projects">Projects</a>
+                    <a class="nav-link" href="#projects">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Resume</a>
                 </li>
             </ul>
         </div>
